3ef715a8e9d115c270a47a4c88de76bdd893942e
Jaime Parra <jp.alejandro01@gmail.com>
2022-08-19 04:43:04 +0000

    unformatted db filea

--- docker_fastapi/app/database.py
+++ docker_fastapi/app/database.py
@@ -1,16 +1,13 @@
 from sqlalchemy import create_engine
-
-
 from sqlalchemy.ext.declarative import declarative_base
-
-
-
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL =            "mysql+mysqlconnector://root:root@mysql/fastapi"
+'''This is a test docstring for this code'''
+
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:root@mysql/fastapi"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine =                create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal     =              sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base =                               declarative_base()
+Base = declarative_base()
