2f2edfa54627336b88190eb026c982f4e660952b
Jaime Parra <jp.alejandro01@gmail.com>
2022-08-19 04:39:18 +0000

    actions test

--- docker_fastapi/app/database.py
+++ docker_fastapi/app/database.py
@@ -13,4 +13,4 @@ SQLALCHEMY_DATABASE_URL =            "mysql+mysqlconnector://root:root@mysql/fas
 engine =                create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal     =              sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base =                              declarative_base()
+Base =                               declarative_base()
